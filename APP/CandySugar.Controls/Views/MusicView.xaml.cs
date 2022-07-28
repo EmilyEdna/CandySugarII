80492d39e1e3c2ae58f220f035a6e89a81333aff
EmilyEdna <847432003@qq.com>
2022-07-28 14:51:54 +0800

    MAUI 7.6

--- APP/CandySugar.Controls/Views/MusicView.xaml.cs
+++ APP/CandySugar.Controls/Views/MusicView.xaml.cs
@@ -4,17 +4,52 @@ namespace CandySugar.Controls.Views;
 
 public partial class MusicView : ContentPage
 {
-	public MusicView()
-	{
-		InitializeComponent();
-        MessagingCenter.Subscribe<MusicViewModel,bool>(this, "QueryStart", (sender, args) => {
-			if (args)
-			{
-				SearchHandle.Dispatcher.Dispatch(() =>
-				{
-					SearchHandle.SetIsFocused(false);
+    public MusicView()
+    {
+        InitializeComponent();
+        //ModuleBtn.Source = GetImageSource();
+        MessagingCenter.Subscribe<MusicViewModel, bool>(this, "QueryStart", (sender, args) =>
+        {
+            if (args)
+            {
+                SearchHandle.Dispatcher.Dispatch(() =>
+                {
+                    SearchHandle.SetIsFocused(false);
                 });
             }
-		});
+        });
+    }
+
+    FontImageSource GetImageSource()
+    {
+        if (Module)
+        {
+            Module = false;
+            return new FontImageSource
+            {
+                Color = Colors.Black,
+                FontFamily = "Thin",
+                Glyph = FontIcon.ArrowsRepeat,
+                Size = 15,
+            };
+        }
+        else
+        {
+            Module = true;
+            return new FontImageSource
+            {
+                Color = Colors.Black,
+                FontFamily = "Thin",
+                Glyph = FontIcon.ArrowsRepeat1,
+                Size = 15,
+            };
+
+        }
+    }
+
+    bool Module = true;
+    private void TapEvent(object sender, EventArgs e)
+    {
+        //ModuleBtn.Source = GetImageSource();
     }
 }
