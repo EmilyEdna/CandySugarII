0ea5bc2f6c662126dbcbbad8b3ead5bc5bb870f1
EmilyEdna <847432003@qq.com>
2022-09-22 17:06:13 +0800

    WEB 2.5

---
+++ Web/CandySugar.Library/SdkLog.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Logging;
+using Sdk.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandySugar.Library
+{
+    public class SdkLog : ISdkLogger
+    {
+        public Task Log(string className, string methodInfo, Exception message)
+        {
+            Furion.Logging.Log.CreateLogger("SDK日志").LogError(message.Message, new { ClassName= className,Method= methodInfo });
+            return Task.CompletedTask;
+        }
+    }
+}
