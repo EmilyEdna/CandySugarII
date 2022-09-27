54be6f53ec8269165a43a6c3af93adf15fb5f557
EmilyEdna <847432003@qq.com>
2022-09-27 15:02:28 +0800

    WEB 3.4

---
+++ Web/CandySugar.Library/Logic/Service/HnimeService.cs
@@ -0,0 +1,14 @@
+﻿using CandySugar.Library.Logic.IService;
+using Furion.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandySugar.Library.Logic.Service
+{
+    public class HnimeService:DbContext, IHnimeService,IScoped
+    {
+    }
+}
