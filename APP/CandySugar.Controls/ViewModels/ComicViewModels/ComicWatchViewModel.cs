fafda9d054f2c17ccec6c80d7621feab587d394e
EmilyEdna <847432003@qq.com>
2022-10-14 11:53:31 +0800

    MAUI 9.6

--- APP/CandySugar.Controls/ViewModels/ComicViewModels/ComicWatchViewModel.cs
+++ APP/CandySugar.Controls/ViewModels/ComicViewModels/ComicWatchViewModel.cs
@@ -8,18 +8,33 @@ namespace CandySugar.Controls.ViewModels.ComicViewModels
 {
     public class ComicWatchViewModel : BaseViewModel
     {
+        static List<string> Data;
         public override void ApplyQueryAttributes(IDictionary<string, object> query)
         {
-            var data = query.Values.FirstOrDefault() as List<string>;
-            Img = new ObservableCollection<string>(data);
+            Data = query.Values.FirstOrDefault() as List<string>;
+            this.Page = 0;
+            this.Total = Data.Count;
+            Route = new ObservableCollection<string>
+            {
+                Data.FirstOrDefault()
+            };
         }
         #region 属性
-        ObservableCollection<string> _Img;
-        public ObservableCollection<string> Img
+        ObservableCollection<string> _Route;
+        public ObservableCollection<string> Route
         {
-            get => _Img;
-            set => SetProperty(ref _Img, value);
+            get => _Route;
+            set => SetProperty(ref _Route, value);
         }
+        #endregion
+
+        #region 命令
+        public DelegateCommand LoadMoreAction => new(() =>
+        {
+            this.Page += 1;
+            if (this.Page > this.Total-1) return;
+            Route.Add(Data[this.Page]);
+        });
         #endregion
     }
 }
