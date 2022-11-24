800cedc30d8aa59c5911fe70e48442817fef8a13
EmilyEdna <847432003@qq.com>
2022-11-24 17:41:56 +0800

    MAUI 20.4

--- APPX/CandySugar.Entry/Views/Index.xaml.cs
+++ APPX/CandySugar.Entry/Views/Index.xaml.cs
@@ -1,9 +1,11 @@
+using UraniumUI.Pages;
+
 namespace CandySugar.Entry.Views;
 
-public partial class Index : ContentPage
+public partial class Index : UraniumContentPage
 {
-	public Index()
-	{
-		InitializeComponent();
-	}
+    public Index()
+    {
+        InitializeComponent();
+    }
 }
