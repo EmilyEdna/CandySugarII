285a3518437e6b97efe74a68ffdc2ed599c08ac1
EmilyEdna <847432003@qq.com>
2023-03-01 17:29:15 +0800

    WPF 20.8

--- PCX/CandySugar.EntryUI/ViewModels/IndexViewModel.cs
+++ PCX/CandySugar.EntryUI/ViewModels/IndexViewModel.cs
@@ -1,13 +1,26 @@
 ﻿using Stylet;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using StyletIoC;
 
 namespace CandySugar.EntryUI.ViewModels
 {
     public   class  IndexViewModel: Conductor<IScreen>
     {
+        public IContainer Container;
+        public IWindowManager WindowManager;
+        public IndexViewModel(IContainer Container, IWindowManager WindowManager)
+        {
+            this.Container = Container;
+            this.WindowManager = WindowManager;
+            this.Title = "IndexView";
+        }
+
+        #region Property
+        private string _Title;
+        public string Title
+        {
+            get => _Title;
+            set => SetAndNotify(ref _Title, value);
+        }
+        #endregion
     }
 }
