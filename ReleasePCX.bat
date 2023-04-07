d4b89fb7481bac4a4c43f981b1a21eef297319d4
EmilyEdna <847432003@qq.com>
2023-04-07 21:40:34 +0800

    WPF 26.3

--- ReleasePCX.bat
+++ ReleasePCX.bat
@@ -4,6 +4,7 @@ cd /d %~dp0
 dotnet publish PCX\CandySugar.EntryUI\CandySugar.EntryUI.csproj -c Release -o ..\CandySugarII\2\Release -f net7.0-windows --sc true -r win-x64
 dotnet publish PCX\Component\CandySugar.LightNovel\CandySugar.LightNovel.csproj -c Release -o ..\CandySugarII\2\Release -f net7.0-windows --sc true -r win-x64
 dotnet publish PCX\Component\CandySugar.Music\CandySugar.Music.csproj -c Release -o ..\CandySugarII\2\Release -f net7.0-windows --sc true -r win-x64
+dotnet publish PCX\Component\CandySugar.WallPaper\CandySugar.WallPaper.csproj -c Release -o ..\CandySugarII\2\Release -f net7.0-windows --sc true -r win-x64
 
 rd /S /Q PCX\CandySugar.EntryUI\obj PCX\CandySugar.EntryUI\bin\Release
 
@@ -18,6 +19,8 @@ rd /S /Q PCX\CandySugar.Com.Style\obj PCX\CandySugar.Com.Style\bin\Release
 rd /S /Q PCX\Component\CandySugar.LightNovel\obj PCX\Component\CandySugar.LightNovel\bin\Release
 
 rd /S /Q PCX\Component\CandySugar.Music\obj PCX\Component\CandySugar.Music\bin\Release
+
+rd /S /Q PCX\Component\CandySugar.WallPaper\obj PCX\Component\CandySugar.WallPaper\bin\Release
 
 xcopy PCX\CandySugar.EntryUI\bin\Debug\net7.0-windows\ffmpeg 2\Release\ffmpeg /e /s
 
